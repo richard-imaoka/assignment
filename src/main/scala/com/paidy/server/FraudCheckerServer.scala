0f15a5d162d3af98998eb956267ea4ea41c80d84
Richard S. Imaoka <richard.e.imaoka@gmail.com>
2017-04-06 09:18:53 +0900

    Take historical avg, fail (HTTP 500) if timeout

--- src/main/scala/com/paidy/server/FraudCheckerServer.scala
+++ src/main/scala/com/paidy/server/FraudCheckerServer.scala
@@ -10,45 +10,79 @@ import akka.util.Timeout
 import com.paidy.authorizations.actors.AddressFraudProbabilityScorer
 import com.paidy.authorizations.actors.AddressFraudProbabilityScorer._
 import com.paidy.domain.Address
+import com.typesafe.config.ConfigFactory
 import spray.json._
 
+import scala.collection.immutable.Queue
 import scala.concurrent.duration._
 import scala.io.StdIn
 import scala.util.{Failure, Success}
 
 trait JsonSupport extends SprayJsonSupport with DefaultJsonProtocol {
   implicit val addressFormat = jsonFormat5(Address) //to marshall/unmarshall (i.e. JSON <-> Address) for 5-parameter case class
-  implicit val whatSoEverFormat = jsonFormat2(ReturnJSON)
+  implicit val returnJSONFormat = jsonFormat2(ReturnJSON)
 }
 
 case class ReturnJSON(status: Boolean, address: Address)
 
+//backend failure -> 500
+//timeout -> 5 seconds -> test
+//timeout -> 500
+//use logger instead of println
+//read ip from config
+//read port from config
+
 object FraudCheckerServer extends Directives with JsonSupport{
+
+  var scoreHistory = Map[String, Queue[Double]]()
+
+  def getHistoricalScoresUpTp10(score: Double, address: Address, history: Map[String, Queue[Double]]): Queue[Double] = {
+    val key = address.toString
+    val historicalScores = history.getOrElse(key, Queue[Double]())
+
+    if(historicalScores.size == 10){
+      val (_, historical9) = historicalScores.dequeue
+      historical9.enqueue(score)
+    }
+    else
+      historicalScores.enqueue(score)
+  }
+
+  def judge(score: Double, address: Address, history: Map[String, Queue[Double]]): Boolean = {
+    val historicalScores: Queue[Double] = getHistoricalScoresUpTp10(score, address, history)
+    val average = historicalScores.foldLeft(0.0)((x, y) => x+y) / historicalScores.size
+    println("average: ", average)
+
+    val key = address.toString
+    scoreHistory = history.updated(key, historicalScores)
+
+    score < 0.78 && average < 0.7
+  }
 
   def main(args: Array[String]) {
 
-    implicit val system = ActorSystem("my-system")
+    implicit val system = ActorSystem("my-system",ConfigFactory.load())
     implicit val materializer = ActorMaterializer()
     implicit val executionContext = system.dispatcher
 
     val scorer = system.actorOf(AddressFraudProbabilityScorer.props)
-    implicit val timeout = Timeout(10 seconds)
+    implicit val timeout = Timeout(5 seconds)
 
     val route =
       path("check") {
         post {
-          entity(as[Address]) { address => // will unmarshal JSON to Order
-            println("received: ", address) //change it to logger
+          entity(as[Address]) { address =>
+            println("received: ", address)
             val fut = scorer ? ScoreAddress(address)
             onComplete(fut){
-              case Success(score) => {
+              case Success(s) => {
+                val score = s.asInstanceOf[Double]
                 println("score: ", score)
-                val status = score.asInstanceOf[Double] >= 0.78
+                val status = judge(score, address, scoreHistory)
                 complete(ReturnJSON(status, address))
               }
               case Failure(ex) => {
-                println(ex)
-                complete("error happened")
+                failWith(ex) // this doesn't expose the error details to the HTTP client
               }
             }
           }
